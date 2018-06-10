37adff5f4b3a6dcb10f214661594139774605ae6
Xuan <948993195@qq.com>
2018-06-11 03:50:49 +0800

    test

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 importScripts('sw-toolbox.js');
 
-toolbox.precache(["index.html","assets/css/main.css","assets/css/noscript.css"]);
+toolbox.precache(["index.html","style/style.css"]);
 
 toolbox.router.get('/images/*', toolbox.cacheFirst);
 
