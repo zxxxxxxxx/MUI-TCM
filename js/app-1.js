65b8bdbc678a91553f23b0d960d4b7dcb8cb0704
Xuan <948993195@qq.com>
2018-06-15 05:20:45 +0800

    add medicine.html

--- js/app-1.js
+++ js/app-1.js
@@ -95,37 +95,46 @@
   // Given an object representing a todo, this will create a list item
   // to display it.
   function createTodoListItem(todo) {
-    var p = document.createElement('p');
-    p.className = 'mui-ellipsis';
-    p.appendChild(document.createTextNode(todo.title));
+    var checkbox = document.createElement('input');
+    checkbox.className = 'toggle';
+    checkbox.type = 'checkbox';
+    checkbox.addEventListener('change', checkboxChanged.bind(this, todo));
+
+    var label = document.createElement('label');
+    label.appendChild(document.createTextNode(todo.title));
+    label.addEventListener('dblclick', todoDblClicked.bind(this, todo));
+
+    var deleteLink = document.createElement('button');
+    deleteLink.className = 'destroy';
+    deleteLink.addEventListener('click', deleteButtonPressed.bind(this, todo));
 
     var divDisplay = document.createElement('div');
-    divDisplay.className = 'mui-media-body';
-    divDisplay.appendChild(document.createTextNode(todo.title));
-    divDisplay.appendChild(p);
+    divDisplay.className = 'view';
+    divDisplay.appendChild(checkbox);
+    divDisplay.appendChild(label);
+    divDisplay.appendChild(deleteLink);
 
-    var img = document.createElement('img');
-    img.className = 'mui-media-object mui-pull-left';
-    img.src = 'images/cbd.jpg';
-
-    var a = document.createElement('a');
-    a.className = 'mui-navigate-right';
-    a.appendChild(img);
-    a.appendChild(divDisplay);
+    var inputEditTodo = document.createElement('input');
+    inputEditTodo.id = 'input_' + todo._id;
+    inputEditTodo.className = 'edit';
+    inputEditTodo.value = todo.title;
+    inputEditTodo.addEventListener('keypress', todoKeyPressed.bind(this, todo));
+    inputEditTodo.addEventListener('blur', todoBlurred.bind(this, todo));
 
     var li = document.createElement('li');
-    li.className = 'mui-table-view-cell mui-media';
-    li.id = 'li_' + todo.title;
-    li.appendChild(a);
+    li.id = 'li_' + todo._id;
+    li.appendChild(divDisplay);
+    li.appendChild(inputEditTodo);
+
+    if (todo.completed) {
+      li.className += 'complete';
+      checkbox.checked = true;
+    }
+
     return li;
   }
 
   function redrawTodosUI(todos) {
-    // var ul = document.getElementById('todo-list');
-    // ul.innerHTML = '';
-    // todos.forEach(function (todo) {
-    //   ul.appendChild(createTodoListItem(todo.doc));
-    // });
     var ul = document.getElementById('todo-list');
     ul.innerHTML = '';
     todos.forEach(function (todo) {
@@ -140,11 +149,11 @@
     }
   }
 
-  // function addEventListeners() {
-  //   newTodoDom.addEventListener('keypress', newTodoKeyPressHandler, false);
-  // }
+  function addEventListeners() {
+    newTodoDom.addEventListener('keypress', newTodoKeyPressHandler, false);
+  }
 
-  // addEventListeners();
+  addEventListeners();
   showTodos();
 
   if (remoteCouch) {
